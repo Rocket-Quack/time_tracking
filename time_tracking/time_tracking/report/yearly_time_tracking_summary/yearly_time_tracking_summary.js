39f2e5e3803eaa83e4bbf749df9e4ff331c4b0e2
RocketQuack <202538874+Rocket-Quack@users.noreply.github.com>
2026-01-18 16:28:39 +0100

    feat(report): add yearly time tracking summary

---
+++ time_tracking/time_tracking/report/yearly_time_tracking_summary/yearly_time_tracking_summary.js
@@ -0,0 +1,28 @@
+frappe.query_reports["Yearly Time Tracking Summary"] = {
+    filters: (() => {
+        const filters = [
+            {
+                fieldname: "year",
+                label: __("Year"),
+                fieldtype: "Int",
+                default: moment().year(),
+                reqd: 1,
+            },
+        ];
+
+        const isAdmin =
+            frappe.user.has_role("System Manager") ||
+            frappe.user.has_role("Time Tracking Admin");
+
+        if (isAdmin) {
+            filters.push({
+                fieldname: "user",
+                label: __("User"),
+                fieldtype: "Link",
+                options: "User",
+            });
+        }
+
+        return filters;
+    })(),
+};
